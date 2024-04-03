62ad6fc983e7ffcb71a47e0301948e9579925367
eva <evafedorova0081@gmail.com>
2024-04-03 19:28:08 +0300

    Добавляем докстринги и функцию, которая делает первую букву заглавной

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def capitalaze_string(text: str):
+    """Буквы стали большими"""
     return text.upper()
+
+def capitalaze_first_letter(text: str):
+    """Первая буква стала заглавной"""
+    return text.title()
